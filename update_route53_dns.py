d058f21efd3f0297367a2e23f5009f5d9ad9ac49
mendhak <smaltaf@msn.com>
2013-06-26 22:33:09 +0100

    Print messages

--- update_route53_dns.py
+++ update_route53_dns.py
@@ -8,8 +8,8 @@ import time
 
 if __name__ == '__main__':
 
-    R53_ZONEID = "Z3H1234124324"
-    R53_DOMAIN = "abc.example.com"
+    R53_ZONEID = "Z3H1J7NXRY8WVM"
+    R53_DOMAIN = "pi.mendhak.com"
 
     zones = {}
     route53 = Route53Connection()
@@ -18,15 +18,21 @@ if __name__ == '__main__':
     new_ip_address = urllib2.urlopen('http://icanhazip.com').read()
     new_ip_address = new_ip_address.replace("\n","")
 
-    if ( recordsets and recordsets[0].name == R53_DOMAIN + '.' and recordsets[0].resource_records):
+    if recordsets and recordsets[0].name == R53_DOMAIN + '.' and recordsets[0].resource_records:
+        print "Found existing recordset."
         old_ip_address = recordsets[0].resource_records[0]
+        print "Old IP address: " + old_ip_address
         changes = ResourceRecordSets(route53, R53_ZONEID)
         change = changes.add_change("DELETE", R53_DOMAIN, "A")
         change.add_value(old_ip_address)
+        print "Deleting..."
         result = changes.commit()
         time.sleep(10)
 
+    print "Creating new recordset"
     changes2 = ResourceRecordSets(route53, R53_ZONEID)
     change2 = changes2.add_change("CREATE", R53_DOMAIN, "A")
+    print "New IP address:" + new_ip_address
     change2.add_value(new_ip_address)
+    print "Saving..."
     result = changes2.commit()
